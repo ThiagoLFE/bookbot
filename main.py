00578d6fca0cc0f547ec4b9f872196abde93a40a
thiagofirered <thiago.rib1905@gmail.com>
2025-12-14 20:14:09 -0300

    add a argument when start for searching info about the book on the path foward the main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words, count_characters, show_count_characters_formatted
+import sys
 
 def get_book_text(file_path):
     with open(file_path, encoding="utf-8") as f:
@@ -6,10 +7,15 @@ def get_book_text(file_path):
     return file_contents
 
 def main():
-    book_content = get_book_text("./books/frankenstein.txt")
-    all_characters = count_characters(book_content)
-    print(f"Found {get_num_words(book_content)} total words")
+    print("Usage: python3 main.py <path_to_book>")
+    if len(sys.argv) >1:
+        book_content = get_book_text(sys.argv[1])
+        all_characters = count_characters(book_content)
+        print(f"Found {get_num_words(book_content)} total words")
 
-    show_count_characters_formatted(all_characters)
+        show_count_characters_formatted(all_characters)
+
+    else:
+        sys.exit(1);
 
 main()
